d90961f85f5885a01dca5a4c6a7174458fc5e31d
Tawhid Islam Anik <taohidislamanik92@gmail.com>
2024-11-13 07:50:07 +0600

    Add files via upload

---
+++ src/Components/Error.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Error = () => {
+  return (
+    <>
+      <body className='bg-white'>
+        <div className="min-h-screen flex items-center justify-center">
+          <img className='' src="https://cdn.dribbble.com/userupload/6028083/file/original-3aa4ba97cfd2956f084570d638c4a9c2.gif" alt="error" />
+        </div>
+      </body>
+    </>
+  )
+}
+
+export default Error;
